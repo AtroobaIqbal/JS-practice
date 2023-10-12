8c4f15e604ec4f91fdd824b2b679be386ecd0a39
AtroobaIQBAL <atroobaiqbal28@gmail.com>
2023-10-12 18:04:32 +0500

    first commit

---
+++ chp 1-5/alert.js
@@ -0,0 +1,9 @@
+//alert ("hello");
+
+//alert ("Error! please enter a valid password");
+
+//alert ("welcome to JS Land \n Happy coding!");
+
+//alert ("welcome to JS Land");
+//alert ( "Happy coding!");
+
