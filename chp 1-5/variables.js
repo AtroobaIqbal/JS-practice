1a071ddf8874745ed3886793b350688ae45e150b
AtroobaIQBAL <atroobaiqbal28@gmail.com>
2023-10-13 13:00:02 +0500

    chp2

---
+++ chp 1-5/variables.js
@@ -0,0 +1,43 @@
+var username;
+
+var myName = 'Atrooba Iqbal';
+
+var message = 'Hello World!';
+//alert(message);
+
+//var name = 'atrooba';
+//var age = 20 ;
+//var uni = 'karachi uni';
+//var sub = 'zoology';
+
+//alert(name);
+//alert(age);
+//alert(uni);
+//alert(sub);
+
+var food = ' pizza \n pizz \n piz \n pi \n pi \n p';
+//alert (food); 
+
+var future = 'you will be a software developer in us ';
+var spouse = 'you married a hansome man';
+var kids = 'and you have 2 kids';
+//alert( future + ' ' + spouse + ' ' + kids);
+
+var email = 'abcd89@gmail.com';
+var msg = 'your email address is';
+//alert( msg + ' ' + email);
+
+var book = ' a smarter way to learn javascript';
+//alert(book);
+
+var name = ' hi my name is atrooba';
+var age ='i am 20 years old' ;
+var uni = 'i am karachi uni student';
+var sub = ' i studied zoology';
+//alert(name);
+//alert(age);
+//alert(uni);
+//alert(sub);
+
+var Happy = 'yay ! i can write html content through javascript';
+document.write (Happy);
