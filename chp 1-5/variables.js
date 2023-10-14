634498b8a9b7ad57c92c07fe3934d945ce4aafae
AtroobaIQBAL <atroobaiqbal28@gmail.com>
2023-10-14 12:41:02 +0500

    chp3-4

--- chp 1-5/variables.js
+++ chp 1-5/variables.js
@@ -1,8 +1,8 @@
-var username;
+//var username;
 
-var myName = 'Atrooba Iqbal';
+//var myName = 'Atrooba Iqbal';
 
-var message = 'Hello World!';
+//var message = 'Hello World!';
 //alert(message);
 
 //var name = 'atrooba';
@@ -15,29 +15,64 @@ var message = 'Hello World!';
 //alert(uni);
 //alert(sub);
 
-var food = ' pizza \n pizz \n piz \n pi \n pi \n p';
+//var food = ' pizza \n pizz \n piz \n pi \n pi \n p';
 //alert (food); 
 
-var future = 'you will be a software developer in us ';
-var spouse = 'you married a hansome man';
-var kids = 'and you have 2 kids';
+//var future = 'you will be a software developer in us ';
+//var spouse = 'you married a hansome man';
+//var kids = 'and you have 2 kids';
 //alert( future + ' ' + spouse + ' ' + kids);
 
-var email = 'abcd89@gmail.com';
-var msg = 'your email address is';
+//var email = 'abcd89@gmail.com';
+//var msg = 'your email address is';
 //alert( msg + ' ' + email);
 
-var book = ' a smarter way to learn javascript';
+//var book = ' a smarter way to learn javascript';
 //alert(book);
 
-var name = ' hi my name is atrooba';
-var age ='i am 20 years old' ;
-var uni = 'i am karachi uni student';
-var sub = ' i studied zoology';
+//var name = ' hi my name is atrooba';
+//var age ='i am 20 years old' ;
+//var uni = 'i am karachi uni student';
+//var sub = ' i studied zoology';
 //alert(name);
 //alert(age);
 //alert(uni);
 //alert(sub);
 
-var Happy = 'yay ! i can write html content through javascript';
-document.write (Happy);
+//var Happy = 'yay ! i can write html content through javascript';
+//document.write (Happy);
+
+//chp 3
+
+//var age ='i am 20 years old' ;
+//alert(age);
+
+//var visit = 'you have visited this site 20 times';
+//alert (visit);
+
+//var birthYear = 'my birth year is 2002 <br> data type of my declared variable is number';
+//document.write (birthYear);
+
+//var clientName = 'john Doe ';
+//var order = 'ordered 5 t-shirts';
+//var store = ' on xyz clothing store';
+//document.write (clientName + order + store);
+
+//chp 4
+
+//var greet = 'hello';
+//var name = 'john';
+//var date = '24';
+//document.write (greet +' '+ name + ' '+ date);
+
+var 1st = 'illegal';
+var full-name = 'ilegal';
+var alert = 'illegl';
+var var = 'illegal';
+var My Name = 'illegal';
+
+var fullName = 'legal';
+var full_name = 'legal';
+var num1 = 'legal';
+var my0name = 'legal';
+var age = 'legal';
