8b60dd31a1dc16f87677defbaf67a34f059de3ac
AtroobaIQBAL <atroobaiqbal28@gmail.com>
2023-11-08 18:07:36 +0500

    ifelse

---
+++ chp 12-13/ifelse.js
@@ -0,0 +1,53 @@
+/*1. Write a program to check whether the given input number is
+divisible by 3 or else show a message “Number is not divisible
+by 3”.
+var divi = +prompt("enter any number which is divisible by 3");
+if (divi % 3 === 0) {
+    document.write("this number is divisible by 3");
+}
+else{
+    document.write("this number is not divisible by 3");
+}
+
+2. Write a program that checks whether the given input is an even
+number or an odd number.
+var num = +prompt("enter a number");
+if (num % 2 === 0) {
+    document.write("it is a even number");
+}
+else {
+    document.write("it is a odd number");
+}
+
+3. Write an if/else statement with the following condition:
+If the variable age is greater than 18, output "Old enough",
+otherwise output "Too young".
+var age = +prompt("enter your age");
+if (age > 18) {
+    document.write("old enough");
+} else{
+    document.write("too young");
+}
+
+4. Write a program that prompts the user for their name, and then
+displays a special greeting to that person if their name is the
+same as yours. If the name entered by the user is anything
+other than your name, your code should not produce any
+output.
+var name = "elsa";
+var clName = prompt("enter your name");
+if (name === clName) {
+    document.write("good luck");
+} 
+
+5. Write a program to check whether the given input number is
+divisible by 3 or not by using Switch Case statements. Show a
+message “Number is not divisible by 3” or “Number is divisible
+by 3”.
+
+
+
+6. Write a program that takes a character(number or string) in a
+variable & checks whether the given input is a number,
+uppercase letter or lower case letter. (Hint: ASCII codes:-
+A=65, Z=90, a=97, z=122)*/
